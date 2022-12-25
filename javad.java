cabe04230ac73090060ba464a9d3ed869ba4e521
Ashwaq Aljanahi <94136081+ashwaqaljanahi2021@users.noreply.github.com>
2022-12-25 15:33:03 -0500

    Add files via upload

---
+++ javad.java
@@ -0,0 +1,38 @@
+class MyFirstApp{
+    public static void main(String[] args){
+        System.out.println("I Rule!");
+        System.out.println("The World");
+    }
+}
+
+
+class Loopy{
+
+    public static void main(String[] args)
+{
+    int x=1;
+    while(x<4)
+    {
+    System.out.println("In the loop");
+    System.out.println("Value of x is "+ x);
+    x=x+1;
+    }
+
+System.out.println("This is after the loop");
+}
+
+}
+
+class Iftest{
+
+public static void main(String[] args)
+{
+    int x=2;
+    if(x==3)
+    
+        System.out.println("x is 3");
+    
+    else
+    System.out.println("nope");
+}
+}
